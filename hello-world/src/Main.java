bb7bee449576ce32f77dda752481d3ce1d479104
ItoRino424 <rino.ito@intloop.com>
2024-11-21 18:33:17 +0900

    first commit

---
+++ hello-world/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // int型の変数 x に 11 を代入
+        int x = 11;
+        
+        // x の値を "x=11" の形式で表示
+        System.out.println("x=" + x);
+    }
+}
