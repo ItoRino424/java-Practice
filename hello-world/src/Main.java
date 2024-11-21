450127ddfe578df3fd3f2cf47c588a7e0069dd20
ItoRino424 <rino.ito@intloop.com>
2024-11-21 18:44:58 +0900

    練習問題1-3

--- hello-world/src/Main.java
+++ hello-world/src/Main.java
@@ -1,9 +1,12 @@
+// int 型の変数 x、y に数値 13、17 を代入し、x、y の値を“x=13,y=17”のように表示するプログラムを作成しなさい。
 public class Main {
     public static void main(String[] args) {
         // int型の変数 x に 11 を代入
-        int x = 11;
+        int x = 13;
+        int y = 17;
         
         // x の値を "x=11" の形式で表示
         System.out.println("x=" + x);
+        System.out.println("y=" + y);
     }
 }
